4e782b3d93ca0ab6d1b52e55b77d346b6fba68a1
Will Williams <wwilliams@indeed.com>
2024-02-26 17:34:14 -0800

    Implements merge sort

--- src/mergeSort.js
+++ src/mergeSort.js
@@ -1,5 +1,55 @@
 function mergeSort(compare, elements) {
+  if (Array.isArray(elements)) {
+    if (elements.length <= 1) {
+      return elements;
+    }
+
+    const middle = Math.floor(elements.length / 2);
+
+    const leftElements = elements.slice(0, middle);
+    const rightElements = elements.slice(middle);
+
+    const leftElementsSorted = mergeSort(compare, leftElements);
+    const rightElementsSorted = mergeSort(compare, rightElements);
+
+    return merge(compare, leftElementsSorted, rightElementsSorted);
+  }
   return elements;
+}
+
+/**
+ * Merges two sorted arrays
+ *
+ * @param compare
+ *  Function to compare elements of the array
+ * @param left
+ *  The left sorted array
+ * @param right
+ *  The right sorted array
+ * @returns {*[]}
+ *  The left and right sorted arrays merged in sorted order
+ */
+
+function merge(compare, left, right) {
+  const sorted = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    const comparison = compare(left[leftIndex], right[rightIndex]);
+
+    if (comparison <= 0) {
+      sorted.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      sorted.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  return sorted.concat(
+    leftIndex < left.length ? left.slice(leftIndex) : right.slice(rightIndex)
+  );
 }
 
 module.exports = mergeSort;
